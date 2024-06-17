703214d46d647ee3b8931b9820f0d5979a3894d0
atad_name <atad_email@email.com>
2024-06-17 13:51:04 +0300

    Edit /welcome-repo-oleg/sample_notebook2.sql
    With prompt: generaate some little query

--- sample_notebook2.sql
+++ sample_notebook2.sql
@@ -1,5 +1,18 @@
 SELECT
-  COUNT(*)
-FROM orders
-WHERE
-  created_at > DATE_ADD(CURDATE(), INTERVAL -30 day)
+  *
+FROM (
+  SELECT
+    author,
+    COUNT(*) AS num_stories
+  FROM hackernews.stories
+  GROUP BY author
+) AS T1
+INNER JOIN (
+  SELECT
+    author,
+    COUNT(*) AS num_stories
+  FROM hackernews.stories
+  GROUP BY author
+) AS T2
+ON T1.author = T2.author
+WHERE T1.num_stories > 1 AND T2.num_stories > 1
