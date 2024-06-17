d9b329190b02755b29683637404a908c0db1a595
atad_name <atad_email@email.com>
2024-06-17 15:03:02 +0300

    Edit /welcome-repo-oleg/sample_notebook2.sql
    With prompt: generate user count

--- sample_notebook2.sql
+++ sample_notebook2.sql
@@ -1,18 +1,10 @@
 SELECT
-  *
+  COUNT(*)
 FROM (
   SELECT
     author,
     COUNT(*) AS num_stories
   FROM hackernews.stories
-  GROUP BY author
-) AS T1
-INNER JOIN (
-  SELECT
-    author,
-    COUNT(*) AS num_stories
-  FROM hackernews.stories
-  GROUP BY author
-) AS T2
-ON T1.author = T2.author
-WHERE T1.num_stories > 1 AND T2.num_stories > 1
+  GROUP BY
+    author
+)
