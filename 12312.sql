0f7feaf2ec572c2e3721ed70219c69677666a6b8
atad_name <atad_email@email.com>
2024-06-17 11:29:06 +0300

    Edit /welcome-repo-oleg/12312.sql
    By promt: gen some example

--- 12312.sql
+++ 12312.sql
@@ -1,1 +1,11 @@
-Error: Couldn't find the project to generate code for
+SELECT
+  *
+FROM (
+  SELECT
+    author,
+    COUNT(*) AS num_stories
+  FROM hackernews.stories
+  GROUP BY author
+  ORDER BY num_stories DESC
+  LIMIT 10
+)
