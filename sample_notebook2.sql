b653a3f88b7a6071cc10edd813942867333f5abd
atad_name <atad_email@email.com>
2024-07-11 14:04:36 +0000

    Create /welcome-repo-oleg/testing.view.lkml manually

--- sample_notebook2.sql
+++ sample_notebook2.sql
@@ -1,11 +1,22 @@
+
+WITH date_ranges AS (
+  SELECT
+    '2024 Last Month' AS period,
+    DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '1 month' AS start_date,
+    DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '1 day' AS end_date
+  UNION ALL
+  SELECT
+    '2023 Same Month' AS period,
+    DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '1 year' - INTERVAL '1 month' AS start_date,
+    DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '1 year' - INTERVAL '1 day' AS end_date
+)
 SELECT
-  COUNT(*)
-FROM public.orders
-WHERE EXTRACT(YEAR FROM created_at) = 2024
-  AND EXTRACT(MONTH FROM created_at) = 2
-UNION ALL
-SELECT
-  COUNT(*)
-FROM public.orders
-WHERE EXTRACT(YEAR FROM created_at) = 2023
-  AND EXTRACT(MONTH FROM created_at) = 2
+  dr.period,
+  COUNT(o.id) AS order_count
+FROM
+  date_ranges dr
+LEFT JOIN public.orders o ON
+  o.created_at >= dr.start_date
+  AND o.created_at <= dr.end_date
+GROUP BY
+  dr.period
